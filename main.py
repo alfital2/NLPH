f6d630a4c923fe97318e661177669258f58aedeb
talalfi <alfital2@gmail.com>
2020-11-17 17:51:21 +0200

    refactored  - adding constants names

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import json
 import pandas as pd
 import os
 
-
+# ------------------------ CONSTANTS
 TOKENIZED = 'tokenized_text'
 SEGMENTED = 'segmented_text'
 LEMMAS = 'lemmas'
 DEPENDENCY_TREE = 'dep_tree'
 MA_LATTICE = 'ma_lattice'
 MD_LATTICE = 'md_lattice'
+OUTPUT = 'output_file.csv'
+# ------------------------
+
 
 text = "בתוך עיניה הכחולות"
 # Escape double quotes in JSON.
@@ -22,26 +25,37 @@ r = requests.post(url, data=_json.encode('utf-8'), headers={'Content-type': 'app
 json_object = r.json()
 
 json_formatted_str = json.dumps(json_object, indent=2, ensure_ascii=False)
-# print(json_formatted_str)
 dict_from_json = json.loads(json_formatted_str)
 
+
+# this function will creat a new dataframe with the column names only. it will now put the data inside the dataframe.
+# adding the dataframe will happen later
 def create_dataframe_with_head_row(row):  # head_row = the first row that contain all the column names:
-
     df = pd.DataFrame(columns=row.keys())
-
     return df
 
 
 def load_data_from_json_to_df(df, data):
     for key, value in data.items():
         df = df.append(value, ignore_index=True)
-
+    df = df.append(pd.Series(), ignore_index=True)
     return df
 
 
-if os.path.exists("output_file.csv"):
-    print("fdsaf")
-    os.remove("output_file.csv")
+def create_tokend_dataframe(string, col_name):
+    splitted_string = string.split()
+    indices = [col_name + " " + str(x) for x in range(len(splitted_string))]
+    dict_of_words = dict(zip(indices, splitted_string))
+    df = pd.DataFrame(columns=indices)
+    df = df.append(dict_of_words, ignore_index=True)
+    df = df.append(pd.Series(), ignore_index=True)
+    return df
+
+
+def delete_file_if_already_exist():
+    if os.path.exists(OUTPUT):
+        os.remove(OUTPUT)
+
 
 dep_tree_df = create_dataframe_with_head_row(dict_from_json[DEPENDENCY_TREE]['0'])
 ma_lattice = create_dataframe_with_head_row(dict_from_json[MA_LATTICE]['0'])
@@ -50,8 +64,13 @@ md_lattice = create_dataframe_with_head_row(dict_from_json[MD_LATTICE]['0'])
 dep_tree_df = load_data_from_json_to_df(dep_tree_df, dict_from_json[DEPENDENCY_TREE])
 ma_lattice = load_data_from_json_to_df(ma_lattice, dict_from_json[MA_LATTICE])
 md_lattice = load_data_from_json_to_df(md_lattice, dict_from_json[MD_LATTICE])
+tokenized_df = create_tokend_dataframe(dict_from_json[TOKENIZED], 'token')
+lemma_df = create_tokend_dataframe(dict_from_json[LEMMAS], 'lemma')
+segment_df = create_tokend_dataframe(dict_from_json[SEGMENTED], 'segment')
 
-dep_tree_df.to_csv('output_file.csv',encoding='utf-8-sig', mode='a')
-ma_lattice.to_csv('output_file.csv',encoding='utf-8-sig', mode='a')
-md_lattice.to_csv("output_file.csv",encoding='utf-8-sig',mode='a')
-
+tokenized_df.to_csv(OUTPUT, encoding='utf-8-sig', mode='a')
+lemma_df.to_csv(OUTPUT, encoding='utf-8-sig', mode='a')
+segment_df.to_csv(OUTPUT, encoding='utf-8-sig', mode='a')
+dep_tree_df.to_csv(OUTPUT, encoding='utf-8-sig', mode='a')
+ma_lattice.to_csv(OUTPUT, encoding='utf-8-sig', mode='a')
+md_lattice.to_csv(OUTPUT, encoding='utf-8-sig', mode='a')
